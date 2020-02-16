b1fcff4c7c2f538c2f058aa4f22b056e0aed1d73
fleshjack <107745450+fleshjack@users.noreply.github.com>
2020-02-16 14:40:33 +0000

    add travis.yml file

---
+++ depends/packages/zeromq.mk
@@ -0,0 +1,5 @@
+package=zeromq
+$(package)_version=4.2.2
+$(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash=5b23f4ca9ef545d5bd3af55d305765e3ee06b9862
