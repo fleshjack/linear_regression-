b8ada41637974d01ac6d9f32258432d994c803a0
fleshjack <107745450+fleshjack@users.noreply.github.com>
2020-03-28 16:49:21 +0000

    Fix incorrect executive rights on `test.js`

---
+++ share/qt/make_windows_icon.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+# create multiresolution windows icon
+ICON_DST=../../src/qt/res/icons/Bws.ico
+
+convert ../../src/qt/res/icons/Bws-16.png ../../src/qt/res/icons/Bws-32.png ../../src/qt/res/icons/Bws-48.png ${ICON_DST}
---
+++ src/bitcoind.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "rpcserver.h"
+#include "rpcclient.h"
+#include "init.h"
+#include <boost/algorithm/string/predicate.hpp>
+
+void WaitForShutdown(boost::thread_group* threadGroup)
+{
+    bool fShutdown = ShutdownRequested();
+    // Tell the main threads to shutdown.
+    while (!fShutdown)
+    {
+        MilliSleep(200);
+        fShutdown = ShutdownRequested();
+    }
+    if (threadGroup)
+    {
+        threadGroup->interrupt_all();
+        threadGroup->join_all();
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// Start
+//
+bool AppInit(int argc, char* argv[])
+{
+    boost::thread_group thre
