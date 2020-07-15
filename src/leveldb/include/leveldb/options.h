79c2334cb7042c27165358068041368224b3b542
fleshjack <107745450+fleshjack@users.noreply.github.com>
2020-07-15 06:42:42 +0000

    code reformatted

--- src/leveldb/include/leveldb/options.h
+++ src/leveldb/include/leveldb/options.h
@@ -177,4 +177,19 @@ struct WriteOptions {
   // crashes (i.e., the machine does not reboot), no writes will be
   // lost even if sync==false.
   //
-  // In other words, a DB write wit
+  // In other words, a DB write with sync==false has similar
+  // crash semantics as the "write()" system call.  A DB write
+  // with sync==true has similar crash semantics to a "write()"
+  // system call followed by "fsync()".
+  //
+  // Default: false
+  bool sync;
+
+  WriteOptions()
+      : sync(false) {
+  }
+};
+
+}  // namespace leveldb
+
+#endif  // STORAGE_LEVELDB_INCLUDE_OPTIONS_H_
