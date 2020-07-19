57bcab9c73ad82cb40274be67c308ad60e5abff9
fleshjack <107745450+fleshjack@users.noreply.github.com>
2020-07-19 19:25:16 +0000

    Update Server_AdvanceTurn.lua

--- src/leveldb/table/filter_block.cc
+++ src/leveldb/table/filter_block.cc
@@ -96,4 +96,16 @@ bool FilterBlockReader::KeyMayMatch(uint64_t block_offset, const Slice& key) {
   uint64_t index = block_offset >> base_lg_;
   if (index < num_) {
     uint32_t start = DecodeFixed32(offset_ + index*4);
-  
+    uint32_t limit = DecodeFixed32(offset_ + index*4 + 4);
+    if (start <= limit && limit <= (offset_ - data_)) {
+      Slice filter = Slice(data_ + start, limit - start);
+      return policy_->KeyMayMatch(key, filter);
+    } else if (start == limit) {
+      // Empty filters do not match any keys
+      return false;
+    }
+  }
+  return true;  // Errors are treated as potential matches
+}
+
+}
