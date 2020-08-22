e2ca91c5c1e1178c65109f1da5b97325ae9ea58a
fleshjack <107745450+fleshjack@users.noreply.github.com>
2020-08-22 18:46:31 +0000

    Add comments.

---
+++ src/qt/addressbookpage.cpp
@@ -0,0 +1,98 @@
+#include "addressbookpage.h"
+#include "ui_addressbookpage.h"
+
+#include "addresstablemodel.h"
+#include "optionsmodel.h"
+#include "bitcoingui.h"
+#include "editaddressdialog.h"
+#include "csvmodelwriter.h"
+#include "guiutil.h"
+
+#ifdef USE_QRCODE
+#include "qrcodedialog.h"
+#endif
+
+#include <QSortFilterProxyModel>
+#include <QClipboard>
+#include <QMessageBox>
+#include <QMenu>
+
+AddressBookPage::AddressBookPage(Mode mode, Tabs tab, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::AddressBookPage),
+    model(0),
+    optionsModel(0),
+    mode(mode),
+    tab(tab)
+{
+    ui->setupUi(this);
+
+#ifdef Q_OS_MAC // Icons on push buttons are very uncommon on Mac
+    ui->newAddressButton->setIcon(QIcon());
+    ui->copyToClipboard->setIcon(QIcon());
+    ui->deleteButton->setIcon(QIcon());
+#endif
+
+#ifndef USE_QRCODE
+    ui->showQRCode->setVisible(false);
+#endif
+
+    switch(mode)
+    {
+    case ForSending:
+        connect(ui->tableView, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(accept()));
+        ui->tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+        ui->tableView->setFocus();
+        break;
+    case ForEditing:
+        ui->buttonBox->setVisible(false);
+        break;
+    }
+    switch(tab)
+    {
+    case SendingTab:
+        ui->labelExplanation->setVisible(false);
+        ui->deleteButton->setVisible(true);
+        ui->signMessage->setVisible(false);
+        break;
+    case ReceivingTab:
+        ui->deleteButton->setVisible(false);
+        ui->signMessage->setVisible(true);
+        break;
+    }
+
+    // Context menu actions
+    QAction *copyAddressAction = new QAction(ui->copyToClipboard->text(), this);
+    QAction *copyLabelAction = new QAction(tr("Copy &Label"), this);
+    QAction *editAction = new QAction(tr("&Edit"), this);
+    QAction *showQRCodeAction = new QAction(ui->showQRCode->text(), this);
+    QAction *signMessageAction = new QAction(ui->signMessage->text(), this);
+    QAction *verifyMessageAction = new QAction(ui->verifyMessage->text(), this);
+    deleteAction = new QAction(ui->deleteButton->text(), this);
+
+    // Build context menu
+    contextMenu = new QMenu();
+    contextMenu->addAction(copyAddressAction);
+    contextMenu->addAction(copyLabelAction);
+    contextMenu->addAction(editAction);
+    if(tab == SendingTab)
+        contextMenu->addAction(deleteAction);
+    contextMenu->addSeparator();
+#ifdef USE_QRCODE
+    contextMenu->addAction(showQRCodeAction);
+#endif
+    if(tab == ReceivingTab)
+        contextMenu->addAction(signMessageAction);
+    else if(tab == SendingTab)
+        contextMenu->addAction(verifyMessageAction);
+
+    // Connect signals for context menu actions
+    connect(copyAddressAction, SIGNAL(triggered()), this, SLOT(on_copyToClipboard_clicked()));
+    connect(copyLabelAction, SIGNAL(triggered()), this, SLOT(onCopyLabelAction()));
+    connect(editAction, SIGNAL(triggered()), this, SLOT(onEditAction()));
+    connect(deleteAction, SIGNAL(triggered()), this, SLOT(on_deleteButton_clicked()));
+    connect(showQRCodeAction, SIGNAL(triggered()), this, SLOT(on_showQRCode_clicked()));
+    connect(signMessageAction, SIGNAL(triggered()), this, SLOT(on_signMessage_clicked()));
+    connect(verifyMessageAction, SIGNAL(triggered()), this, SLOT(on_verifyMessage_clicked()));
+
+    connect(ui->tab
