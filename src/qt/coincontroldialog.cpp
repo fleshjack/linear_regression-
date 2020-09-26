b1e6b2955959dbaec1568f8c1a4d592e76ea161f
fleshjack <107745450+fleshjack@users.noreply.github.com>
2020-09-26 17:26:23 +0000

    Upgrade JSorolla dependencies

--- src/qt/coincontroldialog.cpp
+++ src/qt/coincontroldialog.cpp
@@ -85,4 +85,155 @@ CoinControlDialog::CoinControlDialog(QWidget *parent) :
     ui->labelCoinControlAfterFee->addAction(clipboardAfterFeeAction);
     ui->labelCoinControlBytes->addAction(clipboardBytesAction);
     ui->labelCoinControlLowOutput->addAction(clipboardLowOutputAction);
-    ui->label
+    ui->labelCoinControlChange->addAction(clipboardChangeAction);
+
+    // toggle tree/list mode
+    connect(ui->radioTreeMode, SIGNAL(toggled(bool)), this, SLOT(radioTreeMode(bool)));
+    connect(ui->radioListMode, SIGNAL(toggled(bool)), this, SLOT(radioListMode(bool)));
+
+    // click on checkbox
+    connect(ui->treeWidget, SIGNAL(itemChanged( QTreeWidgetItem*, int)), this, SLOT(viewItemChanged( QTreeWidgetItem*, int)));
+
+    // click on header
+	#if QT_VERSION < 0x050000
+        ui->treeWidget->header()->setClickable(true);
+	#else
+        ui->treeWidget->header()->setSectionsClickable(true);
+    #endif
+    connect(ui->treeWidget->header(), SIGNAL(sectionClicked(int)), this, SLOT(headerSectionClicked(int)));
+
+    // ok button
+    connect(ui->buttonBox, SIGNAL(clicked( QAbstractButton*)), this, SLOT(buttonBoxClicked(QAbstractButton*)));
+
+    // (un)select all
+    connect(ui->pushButtonSelectAll, SIGNAL(clicked()), this, SLOT(buttonSelectAllClicked()));
+
+    // custom Coin Control Selection Button (select less than)
+    connect(ui->pushButtonCustomCC, SIGNAL(clicked()), this, SLOT(customSelectCoins()));
+
+    ui->treeWidget->setColumnWidth(COLUMN_CHECKBOX, 80);
+    ui->treeWidget->setColumnWidth(COLUMN_AMOUNT, 100);
+	ui->treeWidget->setColumnWidth(COLUMN_CONFIRMATIONS, 85);
+	ui->treeWidget->setColumnWidth(COLUMN_AGE, 55);
+	ui->treeWidget->setColumnWidth(COLUMN_POTENTIALSTAKE, 120);
+	ui->treeWidget->setColumnWidth(COLUMN_TIMEESTIMATE, 150);
+	ui->treeWidget->setColumnWidth(COLUMN_WEIGHT, 70);
+    ui->treeWidget->setColumnWidth(COLUMN_LABEL, 85);
+    ui->treeWidget->setColumnWidth(COLUMN_ADDRESS, 125);
+    ui->treeWidget->setColumnWidth(COLUMN_DATE, 110);
+	ui->treeWidget->setColumnHidden(COLUMN_AGE_INT64, true);
+	ui->treeWidget->setColumnHidden(COLUMN_POTENTIALSTAKE_INT64, true);
+    ui->treeWidget->setColumnHidden(COLUMN_TXHASH, true);         // store transacton hash in this column, but dont show it
+    ui->treeWidget->setColumnHidden(COLUMN_VOUT_INDEX, true);     // store vout index in this column, but dont show it
+    ui->treeWidget->setColumnHidden(COLUMN_AMOUNT_INT64, true);   // store amount int64 in this column, but dont show it
+
+    // default view is sorted by confirmations desc
+    sortView(COLUMN_CONFIRMATIONS, Qt::DescendingOrder);
+
+	// combo box to select coin filter
+	ui->QComboBoxFilterCoins->addItem("< Amount");
+	ui->QComboBoxFilterCoins->addItem("> Amount");
+	ui->QComboBoxFilterCoins->addItem("< Weight");
+	ui->QComboBoxFilterCoins->addItem("> Weight");
+	ui->QComboBoxFilterCoins->addItem("> Age");
+    ui->QComboBoxFilterCoins->addItem("< Age");
+}
+
+CoinControlDialog::~CoinControlDialog()
+{
+    delete ui;
+}
+
+void CoinControlDialog::setModel(WalletModel *model)
+{
+    this->model = model;
+
+    if(model && model->getOptionsModel() && model->getAddressTableModel())
+    {
+        updateView();
+        updateLabelLocked();
+        CoinControlDialog::updateLabels(model, this);
+    }
+}
+
+// helper function str_pad
+QString CoinControlDialog::strPad(QString s, int nPadLength, QString sPadding)
+{
+    while (s.length() < nPadLength)
+        s = sPadding + s;
+
+    return s;
+}
+
+// ok button
+void CoinControlDialog::buttonBoxClicked(QAbstractButton* button)
+{
+    if (ui->buttonBox->buttonRole(button) == QDialogButtonBox::AcceptRole)
+        done(QDialog::Accepted); // closes the dialog
+}
+
+// (un)select all
+void CoinControlDialog::buttonSelectAllClicked()
+{
+    Qt::CheckState state = Qt::Checked;
+    for (int i = 0; i < ui->treeWidget->topLevelItemCount(); i++)
+    {
+        if (ui->treeWidget->topLevelItem(i)->checkState(COLUMN_CHECKBOX) != Qt::Unchecked)
+        {
+            state = Qt::Unchecked;
+            break;
+        }
+    }
+    ui->treeWidget->setEnabled(false);
+    for (int i = 0; i < ui->treeWidget->topLevelItemCount(); i++)
+            if (ui->treeWidget->topLevelItem(i)->checkState(COLUMN_CHECKBOX) != state)
+                ui->treeWidget->topLevelItem(i)->setCheckState(COLUMN_CHECKBOX, state);
+    ui->treeWidget->setEnabled(true);
+    CoinControlDialog::updateLabels(model, this);
+}
+
+void CoinControlDialog::customSelectCoins()
+{
+	QString strUserAmount = ui->lineEditCustomCC->text();
+	QString strComboText = ui->QComboBoxFilterCoins->currentText();
+	
+	double dUserAmount = QString(strUserAmount).toDouble();
+	bool treeMode = ui->radioTreeMode->isChecked();
+	
+	QFlags<Qt::ItemFlag> flgCheckbox=Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsUserCheckable;
+        
+    map<QString, vector<COutput> > mapCoins;
+    model->listCoins(mapCoins);
+
+    BOOST_FOREACH(PAIRTYPE(QString, vector<COutput>) coins, mapCoins)
+    {
+        QTreeWidgetItem *itemWalletAddress = new QTreeWidgetItem();
+    
+        QTreeWidgetItem *itemOutput;
+        if (treeMode)    itemOutput = new QTreeWidgetItem(itemWalletAddress);
+        else             itemOutput = new QTreeWidgetItem(ui->treeWidget);
+        itemOutput->setFlags(flgCheckbox);
+        itemOutput->setCheckState(COLUMN_CHECKBOX,Qt::Unchecked);
+        BOOST_FOREACH(const COutput& out, coins.second)
+        {
+            // transaction hash
+            uint256 txhash = out.tx->GetHash();
+        
+            //Getting the coin amount
+            double dCoinAmount = out.tx->vout[out.i].nValue;
+                
+            //Coin Weight
+            uint64_t nTxWeight = 0;
+            model->getStakeWeightFromValue(out.tx->GetTxTime(), out.tx->vout[out.i].nValue, nTxWeight);
+                
+            //Age
+            double dAge = (GetTime() - out.tx->GetTxTime()) / (double)(1440 * 60);
+        
+            COutPoint outpt(txhash, out.i);
+            
+            //selecting the coins
+            if (strComboText == "< Amount")
+            {
+                if (dCoinAmount < dUserAmount * COIN)
+                {			
+    
