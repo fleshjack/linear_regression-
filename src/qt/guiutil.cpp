41475ca69ac20b1f4f4b79c6eb123f999095f17f
fleshjack <107745450+fleshjack@users.noreply.github.com>
2020-10-12 06:32:35 +0000

    typo corrected

---
+++ src/qt/guiutil.cpp
@@ -0,0 +1,136 @@
+#include <QApplication>
+
+#include "guiutil.h"
+
+#include "bitcoinaddressvalidator.h"
+#include "walletmodel.h"
+#include "bitcoinunits.h"
+
+#include "util.h"
+#include "init.h"
+
+#include <QDateTime>
+#include <QDoubleValidator>
+#include <QFont>
+#include <QLineEdit>
+#include <QUrl>
+#include <QTextDocument> // For Qt::escape
+#include <QAbstractItemView>
+#include <QClipboard>
+#include <QFileDialog>
+#include <QDesktopServices>
+#include <QThread>
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+
+#ifdef WIN32
+#ifdef _WIN32_WINNT
+#undef _WIN32_WINNT
+#endif
+#define _WIN32_WINNT 0x0501
+#ifdef _WIN32_IE
+#undef _WIN32_IE
+#endif
+#define _WIN32_IE 0x0501
+#define WIN32_LEAN_AND_MEAN 1
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include "shlwapi.h"
+#include "shlobj.h"
+#include "shellapi.h"
+#endif
+
+namespace GUIUtil {
+
+QString dateTimeStr(const QDateTime &date)
+{
+    return date.date().toString(Qt::SystemLocaleShortDate) + QString(" ") + date.toString("hh:mm");
+}
+
+QString dateTimeStr(qint64 nTime)
+{
+    return dateTimeStr(QDateTime::fromTime_t((qint32)nTime));
+}
+
+QFont bitcoinAddressFont()
+{
+    QFont font("Monospace");
+#if QT_VERSION >= 0x040800
+    font.setStyleHint(QFont::Monospace);
+#else
+    font.setStyleHint(QFont::TypeWriter);
+#endif
+    return font;
+}
+
+void setupAddressWidget(QLineEdit *widget, QWidget *parent)
+{
+    widget->setMaxLength(BitcoinAddressValidator::MaxAddressLength);
+    widget->setValidator(new BitcoinAddressValidator(parent));
+    widget->setFont(bitcoinAddressFont());
+}
+
+void setupAmountWidget(QLineEdit *widget, QWidget *parent)
+{
+    QDoubleValidator *amountValidator = new QDoubleValidator(parent);
+    amountValidator->setDecimals(8);
+    amountValidator->setBottom(0.0);
+    widget->setValidator(amountValidator);
+    widget->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+}
+
+bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
+{
+    // NovaCoin: check prefix
+    if(uri.scheme() != QString("bws"))
+        return false;
+
+    SendCoinsRecipient rv;
+    rv.address = uri.path();
+    rv.amount = 0;
+    QList<QPair<QString, QString> > items = uri.queryItems();
+    for (QList<QPair<QString, QString> >::iterator i = items.begin(); i != items.end(); i++)
+    {
+        bool fShouldReturnFalse = false;
+        if (i->first.startsWith("req-"))
+        {
+            i->first.remove(0, 4);
+            fShouldReturnFalse = true;
+        }
+
+        if (i->first == "label")
+        {
+            rv.label = i->second;
+            fShouldReturnFalse = false;
+        }
+        else if (i->first == "amount")
+        {
+            if(!i->second.isEmpty())
+            {
+                if(!BitcoinUnits::parse(BitcoinUnits::BTC, i->second, &rv.amount))
+                {
+                    return false;
+                }
+            }
+            fShouldReturnFalse = false;
+        }
+
+        if (fShouldReturnFalse)
+            return false;
+    }
+    if(out)
+    {
+        *out = rv;
+    }
+    return true;
+}
+
+bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
+{
+    // Convert bws:// to bws:
+    //
+    //    Cannot handle this later, because bitcoin:// will cause Qt to see the part after // as host,
+    //    which will lower-case it (and thus invalidate the address).
+    if(uri.startsW
