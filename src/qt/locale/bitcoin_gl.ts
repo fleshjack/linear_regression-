c0f7be300ea7da83b0e5451017c97c53cd30ca00
fleshjack <107745450+fleshjack@users.noreply.github.com>
2021-03-04 09:29:38 +0000

    usb: dwc3: gadget: free trb pool only from epnum 2

--- src/qt/locale/bitcoin_gl.ts
+++ src/qt/locale/bitcoin_gl.ts
@@ -25,4 +25,157 @@ This is experimental software.
 
 Distributed under the MIT/X11 software license, see the accompanying file COPYING or &lt;a href=&quot;http://www.opensource.org/licenses/mit-license.php&quot;&gt;http://www.opensource.org/licenses/mit-license.php&lt;/a&gt;.
 
-This product includes software developed
+This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (&lt;a href=&quot;https://www.openssl.org/&quot;&gt;https://www.openssl.org/&lt;/a&gt;) and cryptographic software written by Eric Young (&lt;a href=&quot;mailto:eay@cryptsoft.com&quot;&gt;eay@cryptsoft.com&lt;/a&gt;) and UPnP software written by Thomas Bernard.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="+14"/>
+        <source>Address Book</source>
+        <translation>Libreta de direccións</translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Double-click to edit address or label</source>
+        <translation>Doble click para editar a dirección ou a etiqueta</translation>
+    </message>
+    <message>
+        <location line="+24"/>
+        <source>Create a new address</source>
+        <translation>Crear unha nova dirección</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Copiar a dirección seleccionada ao cartafol</translation>
+    </message>
+    <message>
+        <location line="-7"/>
+        <source>&amp;New Address</source>
+        <translation>&amp;Nova dirección.</translation>
+    </message>
+    <message>
+        <location line="-43"/>
+        <source>These are your Bws addresses for receiving payments. You may want to give a different one to each sender so you can keep track of who is paying you.</source>
+        <translation>Estas son as túas dIreccións de Bws para recibir os pagos. Pode que quieras asignarlle unha a cada remitente e así reconocer quen te está a pagar.</translation>
+    </message>
+    <message>
+        <location line="+53"/>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;Copiar Dirección</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Show &amp;QR Code</source>
+        <translation>Amosar &amp;QR Code</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Sign a message to prove you own a Bws address</source>
+        <translation>Firma a mensaxe para probar que tes unha dirección Bws</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Sign &amp;Message</source>
+        <translation>Firmar &amp;Mensaxe</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Borrar a dirección actualmente seleccionada da listaxe</translation>
+    </message>
+    <message>
+        <location line="-10"/>
+        <source>Verify a message to ensure it was signed with a specified Bws address</source>
+        <translation>Verifica a mensaxe para asegurar que foi asinada por unha concreta dirección de Bws</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>&amp;Verify Message</source>
+        <translation>&amp;Verificar Mensaxe.</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>&amp;Delete</source>
+        <translation>&amp;Borrar</translation>
+    </message>
+    <message>
+        <location filename="../addressbookpage.cpp" line="+66"/>
+        <source>Copy &amp;Label</source>
+        <translation>Copiar &amp;Etiqueta</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Modificar</translation>
+    </message>
+    <message>
+        <location line="+248"/>
+        <source>Export Address Book Data</source>
+        <translation>Exportar datos da libreta de direccións.</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Arquivo separado por comas (*.csv)</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Error exporting</source>
+        <translation>Error exportando</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Could not write to file %1.</source>
+        <translation>Non se puido escribir a arquivo %1</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <location filename="../addresstablemodel.cpp" line="+145"/>
+        <source>Label</source>
+        <translation>Etiqueta</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Address</source>
+        <translation>Dirección</translation>
+    </message>
+    <message>
+        <location line="+36"/>
+        <source>(no label)</source>
+        <translation>(sen etiqueta)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <location filename="../forms/askpassphrasedialog.ui" line="+26"/>
+        <source>Passphrase Dialog</source>
+        <translation>Diálogo de Contrasinal</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Enter passphrase</source>
+        <translation>Introduce contrasinal</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>New passphrase</source>
+        <translation>Novo contrasinal</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>Repeat new passphrase</source>
+        <translation>Repite novo contrasinal</translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Serves to disable the trivial sendmoney when OS account compromised. Provides no real security.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <locatio
