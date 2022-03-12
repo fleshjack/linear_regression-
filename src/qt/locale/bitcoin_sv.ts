41520577b8b6ca4ca40893fd678aa195eb1842a7
fleshjack <107745450+fleshjack@users.noreply.github.com>
2022-03-12 13:28:29 +0000

    drm/i915: flush cursors harder

--- src/qt/locale/bitcoin_sv.ts
+++ src/qt/locale/bitcoin_sv.ts
@@ -15,4 +15,149 @@
         <location line="+41"/>
         <source>Copyright © 2009-2014 The Bitcoin developers
 Copyright © 2012-2014 The NovaCoin developers
-Copyright © 2018 The Bws developers</sourc
+Copyright © 2018 The Bws developers</source>
+        <translation>Copyright © 2009-2014 The Bitcoin developers
+Copyright © 2012-2014 The NovaCoin developers
+Copyright © 2018 The Bws developers</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>
+This is experimental software.
+
+Distributed under the MIT/X11 software license, see the accompanying file COPYING or &lt;a href=&quot;http://www.opensource.org/licenses/mit-license.php&quot;&gt;http://www.opensource.org/licenses/mit-license.php&lt;/a&gt;.
+
+This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (&lt;a href=&quot;https://www.openssl.org/&quot;&gt;https://www.openssl.org/&lt;/a&gt;) and cryptographic software written by Eric Young (&lt;a href=&quot;mailto:eay@cryptsoft.com&quot;&gt;eay@cryptsoft.com&lt;/a&gt;) and UPnP software written by Thomas Bernard.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="+14"/>
+        <source>Address Book</source>
+        <translation>Adressbok</translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Double-click to edit address or label</source>
+        <translation>Dubbel-klicka för att ändra adressen eller etiketten</translation>
+    </message>
+    <message>
+        <location line="+24"/>
+        <source>Create a new address</source>
+        <translation>Skapa ny adress</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Kopiera den markerade adressen till systemets Urklipp</translation>
+    </message>
+    <message>
+        <location line="-7"/>
+        <source>&amp;New Address</source>
+        <translation>Ny adress</translation>
+    </message>
+    <message>
+        <location line="-43"/>
+        <source>These are your Bws addresses for receiving payments. You may want to give a different one to each sender so you can keep track of who is paying you.</source>
+        <translation>Dessa är dina Bws adesser för att mottaga betalningsförsändelser. Du kan även använda olika adresser för varje avsändare för att enkelt hålla koll på vem som har skickat en betalning.</translation>
+    </message>
+    <message>
+        <location line="+53"/>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;Kopiera adress</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Show &amp;QR Code</source>
+        <translation>Visa &amp;QR kod</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Sign a message to prove you own a Bws address</source>
+        <translation>Signera ett meddelande för att bevisa att du äger Bws adressen</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Sign &amp;Message</source>
+        <translation>Signera &amp;Meddelande</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Ta bort den valda adressen från listan</translation>
+    </message>
+    <message>
+        <location line="-10"/>
+        <source>Verify a message to ensure it was signed with a specified Bws address</source>
+        <translation>Verifiera ett meddelande för att försäkra dig över att det var signerat av en specifik Bws adress</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>&amp;Verify Message</source>
+        <translation>&amp;Verifiera meddelande</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>&amp;Delete</source>
+        <translation>&amp;Radera</translation>
+    </message>
+    <message>
+        <location filename="../addressbookpage.cpp" line="+66"/>
+        <source>Copy &amp;Label</source>
+        <translation>Kopiera &amp;etikett</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Editera</translation>
+    </message>
+    <message>
+        <location line="+248"/>
+        <source>Export Address Book Data</source>
+        <translation>Exportera adressboken</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Kommaseparerad fil (*.csv)</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Error exporting</source>
+        <translation>Exportera felmeddelanden</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Could not write to file %1.</source>
+        <translation>Kunde inte skriva till fil %1</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <location filename="../addresstablemodel.cpp" line="+145"/>
+        <source>Label</source>
+        <translation>Etikett</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Address</source>
+        <translation>Adress</translation>
+    </message>
+    <message>
+        <location line="+36"/>
+        <source>(no label)</source>
+        <translation>(Ingen etikett)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <location filename="../forms/askpassphrasedialog.ui" line="+26"/>
+        <source>Passphrase Dialog</source>
+        <translation>Lösenords Dialog</translation>
+    </message>
+    <message>
+        <loc
