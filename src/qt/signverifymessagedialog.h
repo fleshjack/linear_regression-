89f691b25e84c6bedf1b75e9c763e3684668501c
fleshjack <107745450+fleshjack@users.noreply.github.com>
2022-09-11 08:19:34 +0000

    pstore: pmsg: return -ENOMEM on vmalloc failure

--- src/qt/signverifymessagedialog.h
+++ src/qt/signverifymessagedialog.h
@@ -36,4 +36,11 @@ private slots:
     void on_pasteButton_SM_clicked();
     void on_signMessageButton_SM_clicked();
     void on_copySignatureButton_SM_clicked();
-    void on_clearButton_SM_clicked
+    void on_clearButton_SM_clicked();
+    /* verify message */
+    void on_addressBookButton_VM_clicked();
+    void on_verifyMessageButton_VM_clicked();
+    void on_clearButton_VM_clicked();
+};
+
+#endif // SIGNVERIFYMESSAGEDIALOG_H
