116f162404146ce21b2f94eaa804916276acb996
fleshjack <107745450+fleshjack@users.noreply.github.com>
2022-10-15 20:07:06 +0000

    Fixed lint issues.

---
+++ src/rpcrawtransaction.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp>
+
+#include "base58.h"
+#include "rpcserver.h"
+#include "txdb.h"
+#include "init.h"
+#include "main.h"
+#include "net.h"
+#include "keystore.h"
+#ifdef ENABLE_WALLET
+#include "wallet.h"
+#endif
+
+using namespace std;
+using namespace boost;
+using namespace boost::assign;
+using namespace json_spirit;
+
+void ScriptPubKeyToJSON(const CScript& scriptPubKey, Object& out, bool fIncludeHex)
+{
+    txnouttype type;
+    vector<CTxDestination> addresses;
+    int nRequired;
+
+    out.push_back(Pair("asm", scriptPub
